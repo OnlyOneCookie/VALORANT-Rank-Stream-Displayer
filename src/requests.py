58bd80f3eddad8c68ac56b81d55d5a46e361027d
OnlyOneCookie <=>
2021-11-24 20:08:55 +0100

    rework for streaming support

--- src/requests.py
+++ src/requests.py
@@ -1,13 +1,11 @@
 import base64
 import json
 import time
-
 import requests
 from colr import color
 import os
 
 from src.logs import Logging
-
 
 class Requests:
     def __init__(self, version):
@@ -30,13 +28,13 @@ class Requests:
     def check_version(self):
         # checking status
         rStatus = requests.get(
-            "https://raw.githubusercontent.com/isaacKenyon/VALORANT-rank-yoinker/main/status.json").json()
+            "https://raw.githubusercontent.com/OnlyOneCookie/VALORANT-Rank-Stream-Displayer/main/status.json").json()
         if not rStatus["status_ok"] or rStatus["print_message"]:
             status_color = (255, 0, 0) if not rStatus["status_ok"] else (0, 255, 0)
             print(color(rStatus["message"], fore=status_color))
 
         # checking for latest release
-        r = requests.get("https://api.github.com/repos/isaacKenyon/VALORANT-rank-yoinker/releases")
+        r = requests.get("https://api.github.com/repos/OnlyOneCookie/VALORANT-Rank-Stream-Displayer/releases")
         json_data = r.json()
         release_version = json_data[0]["tag_name"]  # get release version
         link = json_data[0]["assets"][0]["browser_download_url"]  # link for the latest release
@@ -47,7 +45,7 @@ class Requests:
     def check_status(self):
         # checking status
         rStatus = requests.get(
-            "https://raw.githubusercontent.com/isaacKenyon/VALORANT-rank-yoinker/main/status.json").json()
+            "https://raw.githubusercontent.com/OnlyOneCookie/VALORANT-Rank-Stream-Displayer/main/status.json").json()
         if not rStatus["status_ok"] or rStatus["print_message"]:
             status_color = (255, 0, 0) if not rStatus["status_ok"] else (0, 255, 0)
             print(color(rStatus["message"], fore=status_color))
@@ -151,5 +149,3 @@ class Requests:
                 "User-Agent": "ShooterGame/13 Windows/10.0.19043.1.256.64bit"
             }
         return headers
-
-
